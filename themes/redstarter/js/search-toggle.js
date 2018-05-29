e9dd8d06bdd54b5c8ec325be93eb466b43b0c8bd
ephie <ephielam@gmail.com>
2018-05-29 18:11:00 +0100

    deadline

--- themes/redstarter/js/search-toggle.js
+++ themes/redstarter/js/search-toggle.js
@@ -1,23 +1,3 @@
-// (function($) {
-
-// 	$(function() {
-// 		$('search-toggle').on('click', function(event) {
-// 			event.preventDefault();
-
-// 			var $label $(this).siblings('label');
-// 			$label.animate({width:'toggle'}); 
-// 			$label.children('[type="search"]').focus();
-// 		});	
-
-// 		$('.search-field').on('blur', function() {
-// 			if ($(this).val() === '') {
-// 				$('.header-search label').animate({width: 'hide'});
-// 			}
-// 		});
-// 	});
-
-// })(jQuery);
-
 (function($) {
     
     /**
